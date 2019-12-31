f16562dbd7f4acdcaa1f318e52fd0c1e735888f9
jiangbo <197463061@qq.com>
2019-12-31 08:55:45 +0800

    使用chai.spy监听回调函数

--- src/app.js
+++ src/app.js
@@ -16,6 +16,9 @@ new Vue({
 
 // 单元测试
 import chai from "chai";
+import spies from "chai-spies";
+chai.use(spies);
+
 const expect = chai.expect;
 {
   const Constructor = Vue.extend(Button);
@@ -88,9 +91,10 @@ const expect = chai.expect;
     }
   });
   vm.$mount();
-  vm.$on("click", function() {
-    console.log(1);
-  });
+  let spy = chai.spy(function() {});
+  vm.$on("click", spy);
+  // 希望这个函数被执行
   let button = vm.$el;
   button.click();
+  expect(spy).to.have.been.called();
 }
