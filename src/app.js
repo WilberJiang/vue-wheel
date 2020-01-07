15a0922870dc840a2c3cd7424f88f85151c7c1d2
jiangbo <197463061@qq.com>
2020-01-07 15:50:54 +0800

    添加input.vue及其样式

--- src/app.js
+++ src/app.js
@@ -1,11 +1,13 @@
 import Vue from "vue";
 import Button from "./button";
 import Icon from "./icon";
-import ButtonGroup from "./button-group.vue";
+import ButtonGroup from "./button-group";
+import Input from "./input";
 
 Vue.component("g-button", Button);
 Vue.component("g-icon", Icon);
 Vue.component("g-button-group", ButtonGroup);
+Vue.component("g-input", Input);
 
 new Vue({
   el: "#app",
@@ -14,87 +16,87 @@ new Vue({
   }
 });
 
-// 单元测试
-import chai from "chai";
-import spies from "chai-spies";
-chai.use(spies);
+// // 单元测试
+// import chai from "chai";
+// import spies from "chai-spies";
+// chai.use(spies);
 
-const expect = chai.expect;
-{
-  const Constructor = Vue.extend(Button);
-  const vm = new Constructor({
-    propsData: {
-      icon: "shezhi"
-    }
-  });
-  vm.$mount();
-  let useElement = vm.$el.querySelector("use");
-  let href = useElement.getAttribute("xlink:href");
-  expect(href).to.eq("#icon-shezhi");
-  vm.$el.remove();
-  vm.$destroy();
-}
-{
-  const Constructor = Vue.extend(Button);
-  const vm = new Constructor({
-    propsData: {
-      icon: "shezhi",
-      loading: true
-    }
-  });
-  vm.$mount();
-  let useElement = vm.$el.querySelector("use");
-  let href = useElement.getAttribute("xlink:href");
-  expect(href).to.eq("#icon-jiazaizhong");
-  vm.$el.remove();
-  vm.$destroy();
-}
-{
-  const div = document.createElement("div");
-  document.body.appendChild(div);
-  const Constructor = Vue.extend(Button);
-  const vm = new Constructor({
-    propsData: {
-      icon: "shezhi"
-    }
-  });
-  vm.$mount(div);
-  let svg = vm.$el.querySelector("svg");
-  let { order } = window.getComputedStyle(svg);
-  expect(order).to.eq("1");
-  vm.$el.remove();
-  vm.$destroy();
-}
-{
-  const div = document.createElement("div");
-  document.body.appendChild(div);
-  const Constructor = Vue.extend(Button);
-  const vm = new Constructor({
-    propsData: {
-      icon: "shezhi",
-      iconPosition: "right"
-    }
-  });
-  vm.$mount(div);
-  let svg = vm.$el.querySelector("svg");
-  let { order } = window.getComputedStyle(svg);
-  expect(order).to.eq("2");
-  vm.$el.remove();
-  vm.$destroy();
-}
-{
-  // mock
-  const Constructor = Vue.extend(Button);
-  const vm = new Constructor({
-    propsData: {
-      icon: "shezhi"
-    }
-  });
-  vm.$mount();
-  let spy = chai.spy(function() {});
-  vm.$on("click", spy);
-  // 希望这个函数被执行
-  let button = vm.$el;
-  button.click();
-  expect(spy).to.have.been.called();
-}
+// const expect = chai.expect;
+// {
+//   const Constructor = Vue.extend(Button);
+//   const vm = new Constructor({
+//     propsData: {
+//       icon: "shezhi"
+//     }
+//   });
+//   vm.$mount();
+//   let useElement = vm.$el.querySelector("use");
+//   let href = useElement.getAttribute("xlink:href");
+//   expect(href).to.eq("#icon-shezhi");
+//   vm.$el.remove();
+//   vm.$destroy();
+// }
+// {
+//   const Constructor = Vue.extend(Button);
+//   const vm = new Constructor({
+//     propsData: {
+//       icon: "shezhi",
+//       loading: true
+//     }
+//   });
+//   vm.$mount();
+//   let useElement = vm.$el.querySelector("use");
+//   let href = useElement.getAttribute("xlink:href");
+//   expect(href).to.eq("#icon-jiazaizhong");
+//   vm.$el.remove();
+//   vm.$destroy();
+// }
+// {
+//   const div = document.createElement("div");
+//   document.body.appendChild(div);
+//   const Constructor = Vue.extend(Button);
+//   const vm = new Constructor({
+//     propsData: {
+//       icon: "shezhi"
+//     }
+//   });
+//   vm.$mount(div);
+//   let svg = vm.$el.querySelector("svg");
+//   let { order } = window.getComputedStyle(svg);
+//   expect(order).to.eq("1");
+//   vm.$el.remove();
+//   vm.$destroy();
+// }
+// {
+//   const div = document.createElement("div");
+//   document.body.appendChild(div);
+//   const Constructor = Vue.extend(Button);
+//   const vm = new Constructor({
+//     propsData: {
+//       icon: "shezhi",
+//       iconPosition: "right"
+//     }
+//   });
+//   vm.$mount(div);
+//   let svg = vm.$el.querySelector("svg");
+//   let { order } = window.getComputedStyle(svg);
+//   expect(order).to.eq("2");
+//   vm.$el.remove();
+//   vm.$destroy();
+// }
+// {
+//   // mock
+//   const Constructor = Vue.extend(Button);
+//   const vm = new Constructor({
+//     propsData: {
+//       icon: "shezhi"
+//     }
+//   });
+//   vm.$mount();
+//   let spy = chai.spy(function() {});
+//   vm.$on("click", spy);
+//   // 希望这个函数被执行
+//   let button = vm.$el;
+//   button.click();
+//   expect(spy).to.have.been.called();
+// }
---
+++ src/input.vue
@@ -0,0 +1,74 @@
+<template>
+  <div class="wrapper" :class="{error}">
+    <input :value="value" type="text" :disabled="disabled" />
+    <template v-if="error">
+      <g-icon name="tijiaoshibai" class="error-icon"></g-icon>
+      <span class="error-message">{{error}}</span>
+    </template>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    value: {
+      type: String
+    },
+    disabled: {
+      type: Boolean,
+      default: false
+    },
+    error: {
+      type: String
+    }
+  }
+};
+</script>
+
+<style lang="scss" scoped>
+$height: 32px;
+$border-color: #999;
+$border-color-hover: #666;
+$border-radius: 4px;
+$font-size: 12px;
+$box-shadow-color: rgba(0, 0, 0, 0.5);
+$red: #f14530;
+.wrapper {
+  font-size: $font-size;
+  display: inline-flex;
+  align-items: center;
+  :not(:last-child) {
+    margin-right: 0.5em;
+  }
+  input {
+    height: $height;
+    border: 1px solid $border-color;
+    border-radius: $border-radius;
+    padding: 0 8px;
+    font-size: inherit;
+    &:hover {
+      border-color: $border-color-hover;
+    }
+    &:focus {
+      box-shadow: inset 0 1px 3px $box-shadow-color;
+      outline: none;
+    }
+    &[disabled] {
+      border-color: #bbb;
+      color: #bbb;
+      cursor: not-allowed;
+    }
+  }
+  &.error {
+    input {
+      border-color: $red;
+    }
+  }
+  .error-icon {
+    fill: $red;
+  }
+  .error-message {
+    color: $red;
+  }
+}
+</style>
