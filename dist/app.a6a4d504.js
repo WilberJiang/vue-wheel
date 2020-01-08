09991fb6eeb8fb173de3d10399f06a540d0d424d
jiangbo <197463061@qq.com>
2020-01-08 14:01:01 +0800

    基本实现row和col的功能

--- dist/app.a6a4d504.js
+++ dist/app.a6a4d504.js
@@ -12827,7 +12827,20 @@ exports.default = void 0;
 //
 //
 //
-var _default = {};
+var _default = {
+  props: {
+    gutter: {
+      type: [Number, String]
+    }
+  },
+  mounted: function mounted() {
+    var _this = this;
+
+    this.$children.forEach(function (vm) {
+      vm.gutter = _this.gutter;
+    });
+  }
+};
 exports.default = _default;
         var $925c2b = exports.default || module.exports;
       
@@ -12841,7 +12854,18 @@ exports.default = _default;
   var _vm = this
   var _h = _vm.$createElement
   var _c = _vm._self._c || _h
-  return _c("div", { staticClass: "row" }, [_vm._t("default")], 2)
+  return _c(
+    "div",
+    {
+      staticClass: "row",
+      style: {
+        marginLeft: -_vm.gutter / 2 + "px",
+        marginRight: -_vm.gutter / 2 + "px"
+      }
+    },
+    [_vm._t("default")],
+    2
+  )
 }
 var staticRenderFns = []
 render._withStripped = true
@@ -12889,11 +12913,23 @@ exports.default = void 0;
 //
 //
 //
+//
+//
+//
+//
 var _default = {
   props: {
     span: {
       type: [Number, String]
+    },
+    offset: {
+      type: [Number, String]
     }
+  },
+  data: function data() {
+    return {
+      gutter: 0
+    };
   }
 };
 exports.default = _default;
@@ -12911,7 +12947,14 @@ exports.default = _default;
   var _c = _vm._self._c || _h
   return _c(
     "div",
-    { staticClass: "col", class: ["col-" + _vm.span] },
+    {
+      staticClass: "col",
+      class: ["col-" + _vm.span, _vm.offset && "offset-" + _vm.offset],
+      style: {
+        paddingLeft: _vm.gutter / 2 + "px",
+        paddingRight: _vm.gutter / 2 + "px"
+      }
+    },
     [_vm._t("default")],
     2
   )
--- src/col.vue
+++ src/col.vue
@@ -1,5 +1,9 @@
 <template>
-  <div class="col" :class="[`col-${span}`]">
+  <div
+    class="col"
+    :class="[`col-${span}`,offset&&`offset-${offset}`]"
+    :style="{paddingLeft:gutter/2+'px',paddingRight:gutter/2+'px'}"
+  >
     <slot></slot>
   </div>
 </template>
@@ -9,7 +13,15 @@ export default {
   props: {
     span: {
       type: [Number, String]
+    },
+    offset: {
+      type: [Number, String]
     }
+  },
+  data() {
+    return {
+      gutter: 0
+    };
   }
 };
 </script>
@@ -19,11 +31,18 @@ export default {
   height: 100px;
   background: gray;
   width: 50%;
+  padding: 0 10px;
   border: 1px solid red;
   $class-prefix: col-;
   @for $n from 1 through 24 {
     &.#{$class-prefix}#{$n} {
       width: ($n/24) * 100%;
+    }
+  }
+  $class-prefix: offset-;
+  @for $n from 1 through 24 {
+    &.#{$class-prefix}#{$n} {
+      margin-left: ($n/24) * 100%;
     }
   }
 }
